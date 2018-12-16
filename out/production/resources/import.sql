71e50aaa64aa6865e24d4f0f54547ea56966fa30
Talal <talal.tabia@haw-hamburg.de>
2018-12-16 01:13:23 +0100

    git ignore was a shit, now gradle file can be tracked again

---
+++ out/production/resources/import.sql
@@ -0,0 +1,9 @@
+insert into sound(id, filename, title, creatorname, start_position_in_ms, duration_in_ms, ) values (10, 'xy.3gp', 'FirstSound', 'Venom', 0, 10000)
+
+insert into track(id) values (10)
+
+insert into track_sounds(track_id, sounds_id) values (10, 10)
+
+insert into composition(id, title, creatorname, creation_date, status) values (10, 'FirstComposition', 'Riot', '2018-01-27', 'CREATED')
+
+insert into composition_tracks(composition_id, tracks_id) values (10, 10)
