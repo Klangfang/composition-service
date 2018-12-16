01a12cfa3da4971a9d12b749988bdb5f1dac2189
Talal <talal.tabia@haw-hamburg.de>
2018-12-16 01:20:14 +0100

    Fuck what's wrong with u git

---
+++ src/test/java/com/klangfang/core/CompositionIntegrationTest.java
@@ -0,0 +1,39 @@
+package com.klangfang.core;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CompositionIntegrationTest {
+
+    @Autowired
+    private CompositionRepository compositionRepository;
+
+    @Test
+    public void testPersistenceOfSuccess() {
+        Sound sound = new Sound("SoundTitle", "soundxy.3gp", "Riot", 0, 10000);
+        Track track = new Track(Arrays.asList(sound));
+        Composition composition = new Composition("CompositionTitle", "Venom", Arrays.asList(track));
+        Composition afterSave = compositionRepository.save(composition);
+
+        assertThat(afterSave).isNotNull();
+        assertThat(afterSave.getId()).isPositive();
+
+
+        List<Composition> venomsCompositions = compositionRepository.findByCreatorname("Venom");
+
+        assertThat(venomsCompositions).hasSize(1);
+        Composition venomsComposition = venomsCompositions.get(0);
+        assertThat(venomsComposition.getTracks()).hasSize(1);
+        assertThat(venomsComposition.getTracks().get(0).getSounds()).hasSize(1);
+    }
+}
