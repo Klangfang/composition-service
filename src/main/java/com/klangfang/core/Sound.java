76b87daa17970f85c38d373507295067cc302bbe
Talal <talal.tabia@haw-hamburg.de>
2018-12-19 00:48:11 +0100

    update commit message

---
+++ src/main/java/com/klangfang/core/Sound.java
@@ -0,0 +1,64 @@
+package com.klangfang.core;
+
+import javax.persistence.*;
+import javax.validation.constraints.Min;
+
+@Entity
+public class Sound {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String filename;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Min(0)
+    @Column(nullable = false)
+    private int startPositionInMs;
+
+    @Min(0)
+    @Column(nullable = false)
+    private int durationInMs;
+
+    @Column(nullable = false)
+    private String creatorname;
+
+    public Sound() {
+    }
+
+    public Sound(String filename, String title, String creatorname, @Min(0) int startPositionInMs, @Min(0) int durationInMs) {
+        this.filename = filename;
+        this.title = title;
+        this.creatorname = creatorname;
+        this.startPositionInMs = startPositionInMs;
+        this.durationInMs = durationInMs;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public int getStartPositionInMs() {
+        return startPositionInMs;
+    }
+
+    public int getDurationInMs() {
+        return durationInMs;
+    }
+
+    public String getCreatorname() {
+        return creatorname;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
---
+++ src/main/java/com/klangfang/core/Track.java
@@ -0,0 +1,51 @@
+package com.klangfang.core;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@Entity
+public class Track {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private List<Sound> sounds;
+
+    public Track() {}
+
+    public Track(List<Sound> sounds) {
+        this.sounds = sounds;
+    }
+
+    public List<String> getFilenames() {
+        return sounds.stream()
+                .map(s -> s.getFilename())
+                .collect(Collectors.toList());
+    }
+
+    public int getDurationInMs() {
+        int duration = sounds.stream()
+                .filter(Objects::nonNull)
+                .mapToInt(Sound::getDurationInMs)
+                .sum();
+
+        return duration;
+    }
+
+    //TODO Die Reihenfolge durch Startposition setzten und so auch in die Liste speichen
+    public void addSounds(List<Sound> newSounds) {
+        sounds.addAll(newSounds);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public List<Sound> getSounds() {
+        return sounds;
+    }
+}
