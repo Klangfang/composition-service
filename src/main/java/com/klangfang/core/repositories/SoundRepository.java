484ce29caad70a9300a179d56cab872d6e7c1fe3
Talal Tabia <talal.tabia.one@gmail.com>
2019-03-24 14:16:13 +0100

    remove sound repository

--- src/main/java/com/klangfang/core/repositories/SoundRepository.java
+++
@@ -1,7 +0,0 @@
-package com.klangfang.core.repositories;
-
-import com.klangfang.core.entities.Sound;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SoundRepository extends JpaRepository<Sound, Long> {
-}
